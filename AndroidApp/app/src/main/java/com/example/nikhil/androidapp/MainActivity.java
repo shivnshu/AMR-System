bdc352787a8e5405d6f8b4da0efead704b786fe8
Nikhil Sonkul <nasonkul@gmail.com>
2017-04-01 23:52:42 +0530

    Working

--- AndroidApp/app/src/main/java/com/example/nikhil/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/nikhil/androidapp/MainActivity.java
@@ -1,14 +1,6 @@
 package com.example.nikhil.androidapp;
 
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.net.Socket;
-
-import java.net.*;
-import java.util.*;
-import com.illposed.osc.*;
 
 import android.app.Activity;
 import android.content.Context;
@@ -21,39 +13,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.TextView;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.Date;
 import java.util.Enumeration;
-
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
-import java.net.InetAddress;
-import java.net.Socket;
-import android.app.Activity;
-import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-
 
 
     public class MainActivity extends Activity {
@@ -62,16 +30,19 @@ import android.widget.TextView;
         private SensorManager sensorManager;
         private Sensor sensor,sensor_gyro;
         boolean acc_disp = false;
-        TextView text;
         EditText port;
         TextView AX, AY,AZ, GX,GY,GZ;
+        Button button;
+        private boolean connected = false;
+
 
         private float x, y, z;
         TextView infoIp, infoPort;
         TextView textViewState, textViewPrompt;
 
-        static final int UdpServerPORT = 8080;
+        static final int UdpServerPORT = 5000;
         UdpServerThread udpServerThread;
+        DatagramSocket socket;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -83,29 +54,47 @@ import android.widget.TextView;
             textViewPrompt = (TextView) findViewById(R.id.prompt);
 
             port = (EditText) findViewById(R.id._port);
+            //final String Port = port.getText().toString().trim();
+
             AX = (TextView) findViewById(R.id._ax);
             AY = (TextView) findViewById(R.id._ay);
             AZ = (TextView) findViewById(R.id._az);
             GX = (TextView) findViewById(R.id._gx);
             GY = (TextView) findViewById(R.id._gy);
             GZ = (TextView) findViewById(R.id._gz);
+            button = (Button) findViewById(R.id._start_server);
+            button.setOnClickListener(connectListener);
 
             infoIp.setText(getIpAddress());
             infoPort.setText(String.valueOf(UdpServerPORT));
 
             sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
             sensor = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
-            sensor_gyro = sensorManager.getSensorList(Sensor.TYPE_GYROSCOPE).get(0);
+            sensor_gyro = sensorManager.getSensorList(Sensor.TYPE_ROTATION_VECTOR).get(0);
             acc_disp = false;
+            updateState("UDP Server is not running");
         }
 
-        @Override
-        protected void onStart() {
-            udpServerThread = new UdpServerThread(UdpServerPORT);
-            udpServerThread.start();
-            super.onStart();
-        }
+        private Button.OnClickListener connectListener = new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!connected) {
 
+                    button.setText("Stop");
+                    connected = true;
+                    udpServerThread = new UdpServerThread(UdpServerPORT);
+                    udpServerThread.start();
+                }
+                else{
+                    button.setText("Start Server");
+                    udpServerThread.setRunning(false);
+                    updateState("UDP Server is not running");
+                    connected=false;
+                    acc_disp=false;
+                    //socket.close();
+                }
+            }
+        };
 
         private void updateState(final String state) {
             runOnUiThread(new Runnable() {
@@ -128,7 +117,7 @@ import android.widget.TextView;
         private class UdpServerThread extends Thread {
 
             int serverPort;
-            DatagramSocket socket;
+
 
             boolean running;
 
@@ -147,24 +136,24 @@ import android.widget.TextView;
                 running = true;
 
                 try {
-                    updateState("Starting UDP Server");
                     socket = new DatagramSocket(serverPort);
 
                     updateState("UDP Server is running");
-                    Log.e(TAG, "UDP Server is running");
+                    Log.d(TAG, "UDP Server is running");
 
                     while (running) {
                         byte[] buf = new byte[256];
 
                         // receive request
                         DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                        Log.d(TAG, "waiting for connection");
                         socket.receive(packet);     //this code block the program flow
-
+                        Log.d(TAG, "Got connection");
                         // send the response to the client at "address" and "port"
                         InetAddress address = packet.getAddress();
                         int port = packet.getPort();
 
-                        updatePrompt("Request from: " + address + ":" + port + "\n");
+                        updateState("Request from: " + address + ":" + port + "\n");
 
                         String dString = new Date().toString() + "\n"
                                 + "Your address " + address.toString() + ":" + String.valueOf(port);
@@ -250,13 +239,10 @@ import android.widget.TextView;
                 x = event.values[0];
                 y = event.values[1];
                 z = event.values[2];
-                refreshDisplay();
+                //refreshDisplay();
                 AX.setText( "a_x:"+Float.toString(x));
                 AY.setText( "a_y:"+Float.toString(y));
                 AZ.setText( "a_z:"+Float.toString(z));
-                /*GX.setText( "x:"+Float.toString(x));
-                GY.setText( "x:"+Float.toString(x));
-                GZ.setText( "x:"+Float.toString(x));*/
 
             }
         };
@@ -273,16 +259,8 @@ import android.widget.TextView;
                 GX.setText( "g_x:"+Float.toString(event.values[0]));
                 GY.setText( "g_y:"+Float.toString(event.values[1]));
                 GZ.setText( "g_z:"+Float.toString(event.values[2]));
-                refreshDisplay();
 
             }
         };
 
-        private void refreshDisplay() {
-            if (acc_disp == true) {
-                String output = String.format("X:%3.2f m/s^2  |  Y:%3.2f m/s^2  |   Z:%3.2f m/s^2", x, y, z);
-                text.setText(output);
-            }
-
-        }
     }

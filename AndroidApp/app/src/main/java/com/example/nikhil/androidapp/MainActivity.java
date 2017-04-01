00bf2c949a64076fceef6cdf6422071ca573308b
Nikhil Sonkul <nasonkul@gmail.com>
2017-04-01 23:02:55 +0530

    Add gyroscope

--- AndroidApp/app/src/main/java/com/example/nikhil/androidapp/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/nikhil/androidapp/MainActivity.java
@@ -6,6 +6,41 @@ import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 
+import java.net.*;
+import java.util.*;
+import com.illposed.osc.*;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Date;
+import java.util.Enumeration;
+
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 import android.app.Activity;
 import android.content.Context;
 import android.hardware.Sensor;
@@ -20,132 +55,234 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class MainActivity extends Activity {
 
-    public int PORT = 8080;
-    private Button connectPhones;
-    private String serverIpAddress = "192.168.12.231";
-    private boolean connected = false;
-    TextView text;
-    EditText port;
-    EditText ipAdr;
-    private float x,y,z;
-    private SensorManager sensorManager;
-    private Sensor sensor;
-    boolean acc_disp = false;
-    boolean isStreaming = false;
-    PrintWriter out;
-    private static final String TAG = "MainActivity";
+    public class MainActivity extends Activity {
+        private final static String TAG = MainActivity.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        connectPhones = (Button)findViewById(R.id.send);
-        connectPhones.setOnClickListener(connectListener);
-        text=(TextView)findViewById(R.id.textin);
-        port=(EditText)findViewById(R.id.port);
-        ipAdr=(EditText)findViewById(R.id.ipadr);
-        text.setText("Press send to stream acceleration measurement");
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
-        port.setText("8080");
-        ipAdr.setText(serverIpAddress);
-        acc_disp =false;
-    }
+        private SensorManager sensorManager;
+        private Sensor sensor,sensor_gyro;
+        boolean acc_disp = false;
+        TextView text;
+        EditText port;
+        TextView AX, AY,AZ, GX,GY,GZ;
 
+        private float x, y, z;
+        TextView infoIp, infoPort;
+        TextView textViewState, textViewPrompt;
 
+        static final int UdpServerPORT = 8080;
+        UdpServerThread udpServerThread;
 
-    private Button.OnClickListener connectListener = new Button.OnClickListener() {
         @Override
-        public void onClick(View v) {
-            if (!connected) {
-                if (!serverIpAddress.equals("")) {
-                    connectPhones.setText("Stop Streaming");
-                    Thread cThread = new Thread(new ClientThread());
-                    cThread.start();
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            infoIp = (TextView) findViewById(R.id.infoip);
+            infoPort = (TextView) findViewById(R.id.infoport);
+            textViewState = (TextView) findViewById(R.id.state);
+            textViewPrompt = (TextView) findViewById(R.id.prompt);
+
+            port = (EditText) findViewById(R.id._port);
+            AX = (TextView) findViewById(R.id._ax);
+            AY = (TextView) findViewById(R.id._ay);
+            AZ = (TextView) findViewById(R.id._az);
+            GX = (TextView) findViewById(R.id._gx);
+            GY = (TextView) findViewById(R.id._gy);
+            GZ = (TextView) findViewById(R.id._gz);
+
+            infoIp.setText(getIpAddress());
+            infoPort.setText(String.valueOf(UdpServerPORT));
+
+            sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+            sensor = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER).get(0);
+            sensor_gyro = sensorManager.getSensorList(Sensor.TYPE_GYROSCOPE).get(0);
+            acc_disp = false;
+        }
+
+        @Override
+        protected void onStart() {
+            udpServerThread = new UdpServerThread(UdpServerPORT);
+            udpServerThread.start();
+            super.onStart();
+        }
+
+
+        private void updateState(final String state) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textViewState.setText(state);
+                }
+            });
+        }
+
+        private void updatePrompt(final String prompt) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textViewPrompt.append(prompt);
+                }
+            });
+        }
+
+        private class UdpServerThread extends Thread {
+
+            int serverPort;
+            DatagramSocket socket;
+
+            boolean running;
+
+            public UdpServerThread(int serverPort) {
+                super();
+                this.serverPort = serverPort;
+            }
+
+            public void setRunning(boolean running) {
+                this.running = running;
+            }
+
+            @Override
+            public void run() {
+
+                running = true;
+
+                try {
+                    updateState("Starting UDP Server");
+                    socket = new DatagramSocket(serverPort);
+
+                    updateState("UDP Server is running");
+                    Log.e(TAG, "UDP Server is running");
+
+                    while (running) {
+                        byte[] buf = new byte[256];
+
+                        // receive request
+                        DatagramPacket packet = new DatagramPacket(buf, buf.length);
+                        socket.receive(packet);     //this code block the program flow
+
+                        // send the response to the client at "address" and "port"
+                        InetAddress address = packet.getAddress();
+                        int port = packet.getPort();
+
+                        updatePrompt("Request from: " + address + ":" + port + "\n");
+
+                        String dString = new Date().toString() + "\n"
+                                + "Your address " + address.toString() + ":" + String.valueOf(port);
+                        buf = dString.getBytes();
+                        packet = new DatagramPacket(buf, buf.length, address, port);
+                        socket.send(packet);
+
+                    }
+
+                    Log.e(TAG, "UDP Server ended");
+
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    if (socket != null) {
+                        socket.close();
+                        Log.e(TAG, "socket.close()");
+                    }
                 }
             }
-            else{
-                connectPhones.setText("Start Streaming");
-                connected=false;
-                acc_disp=false;
-            }
         }
-    };
 
-    public class ClientThread implements Runnable {
-        Socket socket;
-        public void run() {
+        private String getIpAddress() {
+            String ip = "";
             try {
-                acc_disp=true;
-                PORT = Integer.parseInt(port.getText().toString());
-                serverIpAddress=ipAdr.getText().toString();
-                InetAddress serverAddr = InetAddress.getByName(serverIpAddress);
-                //InetAddress serverAddr = InetAddress.getByName("TURBOBEAVER");
-                socket = new Socket(serverAddr, PORT);
-                connected = true;
-                out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
-                while (connected) {
-                    out.printf("%10.2f\n", z);
-                    //Log.d(TAG, "a");
-                    out.flush();
-                    Thread.sleep(7);
-                }
-            }
-            catch (Exception e) {
+                Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                        .getNetworkInterfaces();
+                while (enumNetworkInterfaces.hasMoreElements()) {
+                    NetworkInterface networkInterface = enumNetworkInterfaces
+                            .nextElement();
+                    Enumeration<InetAddress> enumInetAddress = networkInterface
+                            .getInetAddresses();
+                    while (enumInetAddress.hasMoreElements()) {
+                        InetAddress inetAddress = enumInetAddress.nextElement();
 
-            }
-            finally{
-                try{
-                    acc_disp=false;
-                    connected=false;
-                    connectPhones.setText("Start Streaming");
-                    //out.close();
-                    socket.close();
-                }catch(Exception a){
+                        if (inetAddress.isSiteLocalAddress()) {
+                            ip += "SiteLocalAddress: "
+                                    + inetAddress.getHostAddress() + "\n";
+                        }
+
+                    }
+
                 }
+
+            } catch (SocketException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                ip += "Something Wrong! " + e.toString() + "\n";
             }
+
+            return ip;
         }
-    };
 
-    // ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ // accelerometer classes // ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ //
-
-    private void init_perif(){
-        // smthing
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        sensorManager.registerListener(accelerationListener, sensor,
-                SensorManager.SENSOR_DELAY_FASTEST);
-    }
-
-    @Override
-    protected void onStop() {
-        sensorManager.unregisterListener(accelerationListener);
-        super.onStop();
-    }
-
-    private SensorEventListener accelerationListener = new SensorEventListener() {
+        // ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ // accelerometer classes // ¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤ //
+        private void init_perif() {
+            // smthing
+        }
 
         @Override
-        public void onAccuracyChanged(Sensor sensor, int acc) {
+        protected void onResume() {
+            super.onResume();
+            sensorManager.registerListener(accelerationListener, sensor,
+                    SensorManager.SENSOR_DELAY_FASTEST);
+            sensorManager.registerListener(gyroListener, sensor_gyro,
+                    SensorManager.SENSOR_DELAY_FASTEST);
         }
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            x = event.values[0];
-            y = event.values[1];
-            z = event.values[2];
-            refreshDisplay();
-        }
-    };
 
-    private void refreshDisplay() {
-        if(acc_disp == true){
-            String output = String.format("X:%3.2f m/s^2  |  Y:%3.2f m/s^2  |   Z:%3.2f m/s^2", x, y, z);
-            text.setText(output);
+        @Override
+        protected void onStop() {
+            sensorManager.unregisterListener(accelerationListener);
+            super.onStop();
+        }
+
+        private SensorEventListener accelerationListener = new SensorEventListener() {
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int acc) {
+            }
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                x = event.values[0];
+                y = event.values[1];
+                z = event.values[2];
+                refreshDisplay();
+                AX.setText( "a_x:"+Float.toString(x));
+                AY.setText( "a_y:"+Float.toString(y));
+                AZ.setText( "a_z:"+Float.toString(z));
+                /*GX.setText( "x:"+Float.toString(x));
+                GY.setText( "x:"+Float.toString(x));
+                GZ.setText( "x:"+Float.toString(x));*/
+
+            }
+        };
+
+        private SensorEventListener gyroListener = new SensorEventListener() {
+            @Override
+            public void onAccuracyChanged(Sensor sensor_gyro, int acc) {
+            }
+
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+
+
+                GX.setText( "g_x:"+Float.toString(event.values[0]));
+                GY.setText( "g_y:"+Float.toString(event.values[1]));
+                GZ.setText( "g_z:"+Float.toString(event.values[2]));
+                refreshDisplay();
+
+            }
+        };
+
+        private void refreshDisplay() {
+            if (acc_disp == true) {
+                String output = String.format("X:%3.2f m/s^2  |  Y:%3.2f m/s^2  |   Z:%3.2f m/s^2", x, y, z);
+                text.setText(output);
+            }
+
         }
     }
-}

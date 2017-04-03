0d7434b355fdf8d374b46feaf5bb8ab8fba45ea2
shivnshu <shivanshusinghraj@gmail.com>
2017-04-03 23:10:57 +0530

    Remove unnecessary folders

--- pythonocc-core-0.17.2/ci/conda/run_test.bat
+++
@@ -1,9 +0,0 @@
-cd /D "%SRC_DIR%/test"
-
-python run_tests.py
-python run_examples_as_tests.py
-python core_webgl_unittest.py
-python core_display_pyqt4_unittest.py
-python core_display_pyqt5_unittest.py
-python core_display_pyside_unittest.py
-python core_display_wx_unittest.py

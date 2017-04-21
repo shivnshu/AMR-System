610c806b302909213fc69638f89a814c0e94c0ba
Dipendra Singh <dipendra32singh@gmail.com>
2017-04-22 03:09:54 +0530

    Corrected the ip, port ordering

--- Client/amr_client_lib.py
+++ Client/amr_client_lib.py
@@ -1,24 +1,24 @@
 import socket, json
 
 class amr_connection:
-	def __init__(self, ip, port_receive, port_send):
-		self.ip = ip;
-		self.port_receive = int(port_receive);
-		self.port_send = int(port_send);
-		self.sensor = "";
+	def __init__(self, server_ip, server_port, listener_port):
+		self.server_ip = server_ip;
+		self.server_port = int(server_port);
+		self.listener_port = int(listener_port);
+		self.sensors = [];
 		self.sockSend = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.sockRecv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.sockRecv.bind(("", self.port_receive))
+		self.sockRecv.bind(("", self.listener_port))
 		
 
-	def get_data(self, sensor):	# Give the name of the sensor to receive
-		if(sensor == self.sensor):
+	def get_data(self, sensors):	# TODO: Concerned about performance problems change the implementation in that cast to a seperate listen and send functions
+		if(sensors == self.sensors):
 			data, addr = self.sockRecv.recvfrom(1024)
 			data_json = json.loads(data)
 			return data_json
 		else:
-			self.sensor = sensor
-			self.sockSend.sendto(self.sensor, (self.ip, self.port_send)) 
+			self.sensors = sensors
+			self.sockSend.sendto(self.sensors, (self.server_ip, self.server_port)) 
 			data, addr = self.sockRecv.recvfrom(1024)
 			data_json = json.loads(data)
 			return data_json

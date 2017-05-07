a311c537ecbe27f0fd294b72edd8e23fc400a666
Dipendra Singh <dipendra32singh@gmail.com>
2017-05-07 11:40:18 +0530

    Fixed Javascript errors

--- AndroidServer/app/src/main/assets/js/webSocketClient.js
+++ AndroidServer/app/src/main/assets/js/webSocketClient.js
@@ -1,24 +1,23 @@
-	ws = new WebSocket("ws://" + self.location.hostname + ":8887");
-  
-  var sensorsData;
+ws = new WebSocket("ws://" + self.location.hostname + ":8887");
+var sensorsData;
 
-	ws.onmessage = function(event) {
-    sensorsData = JSON.parse(event.data)
-    //sensorsData = event.data;
-    //console.log(data['sensor']['x']);
-	};
-	
-	ws.onclose = function() {
-		console.log("Socket closed");
-	};
-	
-	ws.onopen = function() {
-		console.log("Connected");
-		ws.send("Hello from " + navigator.userAgent);
-	};
-	
-	//$("#new-message").bind("submit", function(event) {
-		//event.preventDefault();
-		//ws.send($("#message-text").val());
-		//$("#message-text").val("");
-	//});
+ws.onmessage = function(event) {
+	sensorsData = JSON.parse(event.data)
+		//sensorsData = event.data;
+		//console.log(data['sensor']['x']);
+};
+
+ws.onclose = function() {
+	console.log("Socket closed");
+};
+
+ws.onopen = function() {
+	console.log("Connected");
+	ws.send("Hello from " + navigator.userAgent);
+};
+
+//$("#new-message").bind("submit", function(event) {
+//event.preventDefault();
+//ws.send($("#message-text").val());
+//$("#message-text").val("");
+//});

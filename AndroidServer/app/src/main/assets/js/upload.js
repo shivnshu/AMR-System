d5d65684df06803d30172aae746e19af872beb8a
Dipendra Singh <dipendra32singh@gmail.com>
2017-05-07 13:46:15 +0530

    Adding upload button

---
+++ AndroidServer/app/src/main/assets/js/upload.js
@@ -0,0 +1,44 @@
+var ws;
+var sensorsData;
+function connectChatServer() {
+	ws = new WebSocket("ws://" + self.location.hostname + ":8887");
+
+	ws.binaryType = "arraybuffer";
+	ws.onopen = function() {
+		alert("Connected.")
+	};
+
+	ws.onmessage = function(evt) {
+		//alert(evt.msg);
+		sensorsData = JSON.parse(evt.data);
+	};
+
+	ws.onclose = function() {
+		alert("Connection is closed...");
+	};
+	ws.onerror = function(e) {
+		alert(e.msg);
+	}
+
+}
+
+function sendFile() {
+	var file = document.getElementById('filename').files[0];
+	ws.send('filename:'+file.name);
+	var reader = new FileReader();
+	var rawData = new ArrayBuffer();            
+	//alert(file.name);
+
+	reader.loadend = function() {
+
+	}
+	reader.onload = function(e) {
+		rawData = e.target.result;
+		ws.send(rawData);
+		ws.send('end');
+	}
+
+	reader.readAsArrayBuffer(file);
+	location.reload();
+
+}
